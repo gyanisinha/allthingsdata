ee65af270e5aaf2589de3061480f75aa0095fe8c
Gyani Sinha <87772005+gyanisinha@users.noreply.github.com>
2024-05-15 17:58:32 +0800

    P2F

---
+++ P2F-SKU-migration/README.md
@@ -0,0 +1,29 @@
+# Power BI P SKU Estate - Automated Discovery
+
+## Introduction
+
+The seamless migration from *Power BI P SKU* to *Fabric F SKU* requires meticulous planning and comprehensive discovery. This post will introduce a PowerShell script that automates the discovery of the current Power BI P SKU estate, providing valuable insights for the migration process.
+
+## Pre-requisites
+
+Before we dive into the script, ensure that you have the following pre-requisites:
+
+1. PowerShell 5.1 or higher
+2. MicrosoftPowerBIMgmt module installed. If not, you can install it using the command `Install-Module -Name MicrosoftPowerBIMgmt`
+3. Admin credentials for Power BI service
+
+## Powershell Script
+
+[PowerBI_Discovery_P_SKU.ps1](P2F-SKU-migration/discovery/PowerBI_Discovery_P_SKU.ps1)
+
+## Considerations
+
+The script begins by connecting to the Power BI service using admin credentials. It then defines the output CSV file path and creates an output object to store the workspace details.
+
+The script retrieves all workspaces and for each workspace, it fetches details such as dashboards, reports, dataflows, datasets, and owners, etc. It also makes a REST API call to verify Fabric items for each workspace.
+
+Finally, the script exports the workspace details to a CSV file. This automated process significantly reduces the time and effort required for manual discovery, providing a comprehensive overview of the current Power BI P SKU estate.
+
+## Conclusion
+
+Performing a comprehensive discovery of the current Power BI P SKU estate is a crucial step in planning for the migration to Fabric F SKU. This PowerShell script provides an automated solution for this task, offering valuable insights that can guide the migration process. By leveraging this script, organizations can ensure a smooth and efficient transition, ultimately enhancing their data management and visualization capabilities.
---
+++ P2F-SKU-migration/discovery/PowerBI_Discovery_P_SKU.ps1
@@ -0,0 +1,103 @@
+# **This script can be used for discovery of current PowerBI env, and the information can be used for migration planning to F SKU**
+
+# Install required modules if not already installed and verify permissions
+# Install-Module -Name MicrosoftPowerBIMgmt
+
+# Connect to Power BI service using admin credentials
+Connect-PowerBIServiceAccount
+
+# Define the output CSV file path and output object
+$csvFilePath = ".\PowerBI-Discovery-Report.csv"
+$workspaceDetails = New-Object System.Collections.Generic.List[object]
+
+# Get all workspaces list
+try {
+    # $workspaces = Get-PowerBIWorkspace -Scope Organization -Include All
+    $workspaces = Get-PowerBIWorkspace -All
+} catch {
+    Write-Host "Error getting workspaces: $_"
+    return
+}
+
+# Get access token for Fabric items API
+try {
+    $connection = Get-PowerBIAccessToken
+    $token = $connection.values
+    # Write-Host "Access token: $token"
+} catch {
+    Write-Host "Error getting access token: $_"
+}
+
+
+# get workspace details
+foreach ($workspace in $workspaces){
+    try {
+        #Get dashboard, reports, dataflows and datasets for the current workspace
+        $dasboards = Get-PowerBIDashboard -WorkspaceId $workspace.ID
+        $reports = Get-PowerBIReport -WorkspaceId $workspace.ID
+        $datasets = Get-PowerBIDataset -WorkspaceId $workspace.ID
+        $dataflows = Get-PowerBIDataflow -WorkspaceId $workspace.ID
+        # $migrationStatus = Get-PowerBIWorkspaceMigrationStatus -Id $workspace.ID
+
+        # Get the owners of the workspace
+        $owners = $workspace.Users | Where-Object { $_.AccessRight -eq 'Admin' } | ForEach-Object { $_.UserPrincipalName }
+
+        # Fabric items rest API call
+        $fabricItemsCount = 0
+        
+        # list of Fabric items - check and update from: https://learn.microsoft.com/en-us/rest/api/fabric/admin/items/list-items?tabs=HTTP#itemtype
+        $fabricItemsTypes = @('Lakehouse', 'Warehouse', 'KQLDatabase', 'Notebook', 'DataPipeline', 'Eventstream','KQLQueryset', 'KQLDataConnection', 'MLExperiment', 'MLModel', 'MirroredWarehouse', 'SQLEndpoint', 'SparkJobDefinition')
+
+        try {
+            $endpoint = "https://api.fabric.microsoft.com/v1/admin/items?workspaceId=$($workspace.ID)"
+            $headers = @{"Authorization" = "$token"}
+            $response = Invoke-RestMethod -Uri $endpoint -Headers $headers -Method Get
+            # Write-Host "Response: $response"
+
+            foreach ($item in $response){
+                foreach ($entity in $item.itemEntities){
+                    $itemType = $entity.type
+                    if ($fabricItemsTypes -contains $itemType){
+                        $fabricItemsCount++
+                    }
+                }
+            }
+        } catch {
+            Write-Host "Error verifying Fabric Items for workspace $($workspace.ID): $_"
+        }
+
+        # create custom object
+        $item = [PSCustomObject] @{
+            WorkspaceId = $workspace.ID
+            WorkspaceName = $workspace.Name
+            Type = $workspace.Type
+            State = $workspace.State
+            IsReadOnly = $workspace.IsReadOnly
+            IsOrphaned = $workspace.IsOrphaned
+            IsOnDedicatedCapacity = $workspace.IsOnDedicatedCapacity
+            CapacityId = $workspace.CapacityId
+            Owners = $owners -join ","
+            DashboardsCount = $dasboards.Count
+            ReportsCount = $reports.Count
+            DatasetsCount = $datasets.Count
+            DataflowsCount = $dataflows.Count
+            FabricItemsCount = $fabricItemsCount
+            # MigrationStatus = $migrationStatus.Status
+            }
+          
+
+         $workspaceDetails.Add($item)
+        } catch {
+            Write-Host "Error processing workspace $($workspace.ID): $_"
+        }
+
+}
+
+
+# Export the data to CSV
+try {
+    $workspaceDetails | Export-Csv -Path $csvFilePath -NoTypeInformation
+    Write-Host "Workspace details exported to $csvFilePath"
+} catch {
+    Write-Host "Error exporting to CSV: $_"
+}
